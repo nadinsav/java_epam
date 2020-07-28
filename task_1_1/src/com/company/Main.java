1d4ff74ae60c18b0b40e699ad509fb6db5765377
nadinsav <67588562+nadinsav@users.noreply.github.com>
2020-07-28 13:56:54 +0300

    Add files via upload

---
+++ task_1_1/src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+//Найдите значениефункции:z=((a–3)*b/2)+c.
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        double a;
+        double b;
+        double c;
+        double z;
+
+        a=4.5;
+        b=2.3;
+        c=1.2;
+
+        z=((a-3)*b/2)+c;
+
+        System.out.println("z= " + z);
+    }
+
+}
